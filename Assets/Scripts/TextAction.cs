b344d21a20ae96382a1b444a83806d9cf62ad55f
Akash <akash.w.shah@gmail.com>
2024-01-20 00:57:42 -0500

    Give TextActions restrictions and better arrange file structure for them

--- Assets/Scripts/TextAction.cs
+++ Assets/Scripts/TextAction.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "TextAction", menuName = "TextAction", order = 1)]
+[CreateAssetMenu(fileName = "TextAction", menuName = "TextAction", order = 0)]
 public class TextAction : ScriptableObject
 {
     public string text = "...";
+    [Min(1)]
     public int speed = 2;
     public Action action;
+    [Range(0f, 1f)]
     public float fraction = 0;
 }
