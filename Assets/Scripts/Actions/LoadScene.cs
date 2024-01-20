811a4b4fede67cdec4ec22bb5ff5bb24f270fc8d
Akash <akash.w.shah@gmail.com>
2024-01-20 01:17:03 -0500

    Change names of button actions to just actions

--- Assets/Scripts/Actions/LoadScene.cs
+++ Assets/Scripts/Actions/LoadScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoaderButton : Action
+public class LoadScene : Action
 {
     [SerializeField]
     private string scene;
--- Assets/Scripts/Actions/PlayText.cs
+++ Assets/Scripts/Actions/PlayText.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TextButton : Action
+public class PlayText : Action
 {
     [SerializeField]
     private TextAction[] textActions;
